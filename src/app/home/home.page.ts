3f17885fc633fc99dc66b43dcd68c87dd5d1cb81
Suchatchaya <s.poommarest@gmail.com>
2023-01-18 14:03:16 +0700

    first map

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,135 @@
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+import {
+  GoogleMap,
+  MapInfoWindow,
+  MapGeocoder,
+  MapGeocoderResponse,
+} from '@angular/google-maps';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild('search')
+  public searchElementRef!: ElementRef;
+  @ViewChild('myGoogleMap', { static: false })
+  map!: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false })
+  info!: MapInfoWindow;
+
+  address = '';
+  latitude!: any;
+  longitude!: any;
+  zoom = 12;
+  maxZoom = 15;
+  minZoom = 8;
+  center!: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    mapTypeId: 'hybrid',
+  };
+  markers = [] as any;
+
+  constructor(private ngZone: NgZone, private geoCoder: MapGeocoder) {}
+
+  ngAfterViewInit(): void {
+    // Binding autocomplete to search input control
+    let autocomplete = new google.maps.places.Autocomplete(
+      this.searchElementRef.nativeElement
+    );
+    // Align search box to center
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+      this.searchElementRef.nativeElement
+    );
+    autocomplete.addListener('place_changed', () => {
+      this.ngZone.run(() => {
+        //get the place result
+        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+        //verify result
+        if (place.geometry === undefined || place.geometry === null) {
+          return;
+        }
+
+        console.log({ place }, place.geometry.location?.lat());
+
+        //set latitude, longitude and zoom
+        this.latitude = place.geometry.location?.lat();
+        this.longitude = place.geometry.location?.lng();
+
+        // Set marker position
+        this.setMarkerPosition(this.latitude, this.longitude);
+
+        this.center = {
+          lat: this.latitude,
+          lng: this.longitude,
+        };
+      });
+    });
+  }
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      // Set marker position
+      this.setMarkerPosition(this.latitude, this.longitude);
+    });
+  }
+
+  setMarkerPosition(latitude: any, longitude: any) {
+    // Set marker position
+    this.markers = [
+      {
+        position: {
+          lat: latitude,
+          lng: longitude,
+        },
+        options: {
+          animation: google.maps.Animation.DROP,
+          draggable: true,
+        },
+      },
+    ];
+  }
+
+  eventHandler(event: any, name: string) {
+    // console.log(event, name);
+
+    switch (name) {
+      case 'mapDblclick': // Add marker on double click event
+        break;
+
+      case 'mapDragMarker':
+        break;
+
+      case 'mapDragend':
+        this.getAddress(event.latLng.lat(), event.latLng.lng());
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  getAddress(latitude: any, longitude: any) {
+    this.geoCoder
+      .geocode({ location: { lat: latitude, lng: longitude } })
+      .subscribe((addr: MapGeocoderResponse) => {
+        if (addr.status === 'OK') {
+          if (addr.results[0]) {
+            this.zoom = 12;
+            this.address = addr.results[0].formatted_address;
+          }
+        }
+      });
+  }
+}
